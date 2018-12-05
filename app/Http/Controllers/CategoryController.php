7f97e28a24b2428b753278e839f14ba180028406
shishima123 <shishima21@gmail.com>
2018-12-05 21:37:25 +0700

    fix

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Category;
-use App\Product;
+
 class CategoryController extends Controller
 {
     public function index()
     {
         // $data = Category::with('products')->where('parent_id', 0)->get();
-        $top_selling=Category::join('products', 'categories.id', '=', 'products.category_id')->join('order_items', 'products.id', '=', 'order_items.product_id')->groupby('order_items.product_id')->selectRaw('count(order_items.product_id) as total_items, order_items.*, products.*')->orderBy('total','DESC')->get();
+        $top_selling = Category::join('products', 'categories.id', '=', 'products.category_id')->join('order_items', 'products.id', '=', 'order_items.product_id')->groupBy('order_items.product_id')->selectRaw('count(order_items.product_id) as total_items, order_items.*, products.*')->orderBy('total', 'DESC')->get();
         return $top_selling;
         return view('frontend.index', compact('data'));
     }
