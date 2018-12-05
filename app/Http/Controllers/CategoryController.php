22143c2c3424affca226b63c50828013ca421242
shishima123 <shishima21@gmail.com>
2018-12-05 15:04:33 +0700

    Qeury top_selling

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Category;
-
+use App\Product;
 class CategoryController extends Controller
 {
     public function index()
     {
-        $data = Category::with('products')->where('parent_id', 0)->get();
-        // $top_selling = Product::with('orders')->get()->groupBy('pivot.product_id');
-        // return $data;
+        // $data = Category::with('products')->where('parent_id', 0)->get();
+        $top_selling=Category::join('products', 'categories.id', '=', 'products.category_id')->join('order_items', 'products.id', '=', 'order_items.product_id')->groupby('order_items.product_id')->selectRaw('count(order_items.product_id) as total_items, order_items.*, products.*')->orderBy('total','DESC')->get();
+        return $top_selling;
         return view('frontend.index', compact('data'));
     }
 }
---
+++ readme.txt
@@ -0,0 +1,23 @@
+1. Error: Laravel : Syntax error or access violation: 1055 Error
+    Fix: 
+        In config\database.php --> "mysql" array
+
+        Set 'strict' => false; //to disable all.
+
+        .... or
+        You can leave 'strict' => true and add modes to "mysql" option in
+
+        'mysql' => [
+            ...
+            ....
+            'strict' => true,
+            'modes' => [
+                    //'ONLY_FULL_GROUP_BY', // Disable this to allow grouping by one column
+                    'STRICT_TRANS_TABLES',
+                    'NO_ZERO_IN_DATE',
+                    'NO_ZERO_DATE',
+                    'ERROR_FOR_DIVISION_BY_ZERO',
+                    'NO_AUTO_CREATE_USER',
+                    'NO_ENGINE_SUBSTITUTION'
+                ],
+        ]
