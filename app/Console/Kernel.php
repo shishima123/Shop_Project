a08ee4bffcc91b0a55487b38dbe0f0787b9027e0
shishima123 <shishima21@gmail.com>
2018-12-07 00:54:25 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        'App\Console\Commands\GetTopSelling',
     ];
 
     /**
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('get:topselling')->daily();
     }
 
     /**
@@ -35,7 +34,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,30 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Category;
+use App\Product;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        // $data = Category::with('products')->where('parent_id', 0)->get();
+        $new_products = Category::with('products')->where('parent_id', 0)->get();
+        // return $new_products;
 
-        /*query ra sản phẩm top_selling*/
-        $top_selling = Category::join('products', 'categories.id', '=', 'products.category_id')->join('order_items', 'products.id', '=', 'order_items.product_id')->groupby('order_items.product_id')->selectRaw('count(order_items.product_id) as total_items, order_items.*, products.*')->orderBy('total', 'DESC')->take(5)->get();
+        $top_selling = Product::where('top_selling', 1)->get();
+        return $top_selling;
+        // return view('frontend.index', compact('new_products', 'top_selling'));
 
-        // return $top_selling;
-
-        /*update sản phẩm top_selling vào cột topselling*/
-        foreach ($top_selling as $item) {
-            /*TODO: Viết thêm câu query để set giá trị trong cột top selling=0 sau đó mới update*/
-            Product::find($item->id)->update(['top_selling' => 1]);
-            echo $item->id;
-        }
-
-        // return $top_selling;
-        // return view('frontend.index', compact('data'));
-
-        //TODO: Chuyển đống này qua bên task scheulding để tự động update sau 60'
     }
 }
-// $cart = User::join('orders', 'users.id', '=', 'orders.user_id')->join('order_items', 'orders.id', '=', 'order_items.order_id')->groupBy('users.id')->selectRaw('count(users.id) as total_items, order_items.*, users.*')->get();
+//TODO: Chuyển đống này qua bên task scheulding để tự động update sau 60'
