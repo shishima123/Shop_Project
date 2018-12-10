5b4c2181d1dc1fd2aab311e296b3cc8927a50c1d
shishima123 <shishima21@gmail.com>
2018-12-10 08:01:32 +0700

    update

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,18 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Product;
+use App\Category;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        $new_products = Category::with('products')->where('parent_id', 0)->get();
+        $new_products = Category::with('products')->take(10)->get();
         // return $new_products;
 
         $top_selling = Product::where('top_selling', 1)->get();
-        return $top_selling;
-        // return view('frontend.index', compact('new_products', 'top_selling'));
+
+        // dd($new_products);
+        // foreach ($new_products as $key => $value) {
+        //     echo $new_products;
+        //     echo '<br>';
+        // }
+        // return $new_products;
+        return view('frontend.index', compact('new_products', 'top_selling'));
 
     }
 }
-//TODO: Chuyển đống này qua bên task scheulding để tự động update sau 60'
