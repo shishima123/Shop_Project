a5b6dfbe95026ee5974dd587cc5def01637cef9b
Phuocnd <shishima21@gmail.com>
2019-01-11 23:51:33 +0700

    update secure_asset

--- resources/views/templates/frontend/header.blade.php
+++ resources/views/templates/frontend/header.blade.php
@@ -13,20 +13,20 @@
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
 
 	<!-- Bootstrap -->
-	<link type="text/css" rel="stylesheet" href="{{ asset('css/frontend/bootstrap.min.css') }}" />
+	<link type="text/css" rel="stylesheet" href="{{ secure_asset('css/frontend/bootstrap.min.css') }}" />
 
 	<!-- Slick -->
-	<link type="text/css" rel="stylesheet" href="{{ asset('css/frontend/slick.css') }}" />
-	<link type="text/css" rel="stylesheet" href="{{ asset('css/frontend/slick-theme.css') }}" />
+	<link type="text/css" rel="stylesheet" href="{{ secure_asset('css/frontend/slick.css') }}" />
+	<link type="text/css" rel="stylesheet" href="{{ secure_asset('css/frontend/slick-theme.css') }}" />
 
 	<!-- nouislider -->
-	<link type="text/css" rel="stylesheet" href="{{ asset('css/frontend/nouislider.min.css') }}" />
+	<link type="text/css" rel="stylesheet" href="{{ secure_asset('css/frontend/nouislider.min.css') }}" />
 
 	<!-- Font Awesome Icon -->
-	<link rel="stylesheet" href="{{ asset('css/frontend/font-awesome.min.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('css/frontend/font-awesome.min.css') }}">
 
 	<!-- Custom stlylesheet -->
-	<link type="text/css" rel="stylesheet" href="{{ asset('css/frontend/style.css') }}" />
+	<link type="text/css" rel="stylesheet" href="{{ secure_asset('css/frontend/style.css') }}" />
 </head>
 
 <body>
