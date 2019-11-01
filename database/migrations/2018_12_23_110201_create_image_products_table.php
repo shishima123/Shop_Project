c1b8d82e7418509db529048e78e4418a83e09926
shishima <shishima21@gmail.com>
2019-11-01 14:58:06 +0700

    fix bug migration

--- database/migrations/2018_12_23_110201_create_image_products_table.php
+++ database/migrations/2018_12_23_110201_create_image_products_table.php
@@ -18,7 +18,7 @@ class CreateImageProductsTable extends Migration
             $table->unsignedInteger('product_id');
             $table->foreign('product_id')->references('id')->on('products');
             $table->string('path');
-            $table->number('del_flg');
+            $table->unsignedInteger('del_flg');
             $table->timestamps();
         });
     }
